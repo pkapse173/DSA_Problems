a0bc1528fc9aa801c27b6f39f95c4404d96cdfec
pkapse173 <pkapse173@gmail.com>
2024-04-03 23:34:19 -0400

    Update Min_Stack.java

--- Stack/Min_Stack.java
+++ Stack/Min_Stack.java
@@ -55,13 +55,3 @@ import java.util.Stack;
         return -1;
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
-
