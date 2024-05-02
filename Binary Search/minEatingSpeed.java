bd7019c2380f706c0b23473fe1d65f3d264ac8e0
pkapse173 <pkapse173@gmail.com>
2024-05-01 20:50:43 -0400

    Solved Koko Eating Banana problem with optimized solution

--- Binary Search/minEatingSpeed.java
+++ Binary Search/minEatingSpeed.java
@@ -36,3 +36,38 @@ class Solution {
         return hours <= h;
     }
 }
+
+/*
+ * Approach - Optimized using Binary search
+ * Time Complexity - O(n log n) - n is the number of piles and maxPile is the maximum number of bananas in pile 
+ * Space Complexity - O(1) - Constant space
+ */
+
+ class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int minSpeed = 1;
+
+        int maxSpeed = 0;
+        for (int pile : piles) {
+            maxSpeed = Math.max(maxSpeed, pile);
+        }
+        // Binary Search
+        while (minSpeed < maxSpeed) {
+            int mid = minSpeed + (maxSpeed - minSpeed) / 2;
+
+            if (canEatInTime(piles, h, mid))
+                maxSpeed = mid;
+            else
+                minSpeed = mid + 1;
+        }
+        return minSpeed;
+    }
+
+    private boolean canEatInTime(int[] piles, int h, int speed) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours += (int) Math.ceil((double) pile / speed);
+        }
+        return hours <= h;
+    }
+}
