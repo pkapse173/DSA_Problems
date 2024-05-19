7c0ae5fa386e8fef7ae51c32366051c3a99e984f
pkapse173 <pkapse173@gmail.com>
2024-05-18 22:47:35 -0400

    Solved Linked List Cycle problem

--- LinkedList/LinkedListCycle.java
+++ LinkedList/LinkedListCycle.java
@@ -1,5 +1,43 @@
+
+//Problem: Linked List Cycle
+/*
+ * Input: head = [3,2,0,-4], pos = 1
+ *       
+ * OUTPUT: true
+
+/*
+ * Approach  - Iteration through all nodes
+ * Time Complexity - O(n) - number of nodes 
+ * Space Complexity - O(1) - Constant space
+ */
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 package LinkedList;
 
 public class LinkedListCycle {
-    
+
+    public boolean hasCycle(ListNode head) {
+        
+      if(head == null || head.next == null) return false;
+
+      ListNode slow = head;
+      ListNode fast = head;
+
+      while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast) return true;
+      }
+      return false;
+    }
 }
+    
