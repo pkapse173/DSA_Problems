28780d81930d7c273f9ea65d58567eaae15e4379
kapsep <55979904+pkapse173@users.noreply.github.com>
2024-10-29 19:58:03 -0400

    Solved Increasing Triplet optimized solution

--- 75 Questions/ArraysAndStrings/increasingTriplet.java
+++ 75 Questions/ArraysAndStrings/increasingTriplet.java
@@ -1,3 +1,15 @@
+//Problem: Reverse Vowels of a String
+/*
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+ */
+
+/*
+ * Approach 1 - Brute force approach
+ * Time Complexity - O((n3) -  n is the number elements in an array
+ * Space Complexity - O(1) - No extra space used
+ */
 public class increasingTriplet {
         public boolean increasingTriplet(int[] nums) {
             
@@ -16,3 +28,27 @@ public class increasingTriplet {
     }
 }
 
+/*
+ * Approach 2 - Greedy Approach
+ * Time Complexity - O((n) -  n is the number of elements in an array
+ * Space Complexity - O(1) - No extra space used
+ */
+
+ class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        
+       int first = Integer.MAX_VALUE;
+       int second = Integer.MAX_VALUE;
+
+       for(int num : nums){
+        if(num <= first){
+            first = num;
+        }else if(num <= second){
+            second = num;
+        }else{
+            return true;
+        }
+    }
+    return false;
+    }
+}
